51f764cd60cf8378f673f6e8a425dc0e7e932573
gabrielferreira1d2015@gmail.com <gabrielferreira1d2015@gmail.com>
2023-12-03 19:43:09 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,28 +13,46 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
 
   def send_click(self, **event_args):
-    faringite_predict = anvil.server.call('predict_faringite',
-                                self.age_text.text,
-                                self.temperature_text.text,
-                                self.radio_pain_1.get_group_value(),
-                                self.radio_swollenadp_1.get_group_value(),
-                                self.radio_tender_1.get_group_value(),
-                                self.radio_tonsillarswelling_1.get_group_value(),
-                                self.radio_exudate_1.get_group_value(),
-                                self.radio_cough_1.get_group_value(),
-                                self.radio_rino_1.get_group_value(),
-                                self.radio_conjunctivitis_1.get_group_value(),
-                                self.radio_erythema_1.get_group_value(),
-                                self.radio_abdopain_1.get_group_value(),
-                                self.radio_nauseavomit_1.get_group_value(),
-                                self.radio_headache_1.get_group_value(),
-                                self.radio_scarlet_1.get_group_value(),
-                                self.radio_petechiae_1.get_group_value())
-  
-    self.result_label.visible = True
+    if (self.age_text.text is None 
+        or self.temperature_text.text is None
+        or self.radio_pain_1.get_group_value() is None
+        or self.radio_swollenadp_1.get_group_value() is None
+        or self.radio_tender_1.get_group_value() is None
+        or self.radio_tonsillarswelling_1.get_group_value() is None
+        or self.radio_exudate_1.get_group_value() is None
+        or self.radio_cough_1.get_group_value() is None
+        or self.radio_rino_1.get_group_value() is None
+        or self.radio_conjunctivitis_1.get_group_value() is None
+        or self.radio_erythema_1.get_group_value() is None
+        or self.radio_abdopain_1.get_group_value() is None
+        or self.radio_nauseavomit_1.get_group_value() is None
+        or self.radio_headache_1.get_group_value() is None
+        or self.radio_scarlet_1.get_group_value() is None
+        or self.radio_petechiae_1.get_group_value() is None):
+      alert('Some fields were left empty!')
+    else:
+      faringite_predict = anvil.server.call('predict_faringite',
+                                  self.age_text.text,
+                                  self.temperature_text.text,
+                                  self.radio_pain_1.get_group_value(),
+                                  self.radio_swollenadp_1.get_group_value(),
+                                  self.radio_tender_1.get_group_value(),
+                                  self.radio_tonsillarswelling_1.get_group_value(),
+                                  self.radio_exudate_1.get_group_value(),
+                                  self.radio_cough_1.get_group_value(),
+                                  self.radio_rino_1.get_group_value(),
+                                  self.radio_conjunctivitis_1.get_group_value(),
+                                  self.radio_erythema_1.get_group_value(),
+                                  self.radio_abdopain_1.get_group_value(),
+                                  self.radio_nauseavomit_1.get_group_value(),
+                                  self.radio_headache_1.get_group_value(),
+                                  self.radio_scarlet_1.get_group_value(),
+                                  self.radio_petechiae_1.get_group_value())
     
-    result = "Positive" if faringite_predict == 1 else "Negative"
-    self.result_label.text = result
+      self.result_label.visible = True
+      
+      result = "Positive" if faringite_predict == 1 else "Negative"
+      self.result_label.text = result
 
   def send_show(self, **event_args):
     """This method is called when the Button is shown on the screen"""
